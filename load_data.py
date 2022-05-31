0c969971c288d3a69bfeff52c06db0b6b2d8828c
myndrws <myndrws@gmail.com>
2022-05-31 19:27:00 +0100

    start modularising code

--- load_data.py
+++ load_data.py
@@ -1,30 +1,32 @@
-
 import matplotlib.pyplot as plt
-
 from torch.utils.data import DataLoader
 from torchgeo.datasets import BigEarthNet
+from config import get_args
 
-data_root = 'C:/Users/amy_c/Documents/Python/geo_gans/ben_data'
-batch_size = 64
 
 # load train data from sentinel 2
-train_data = BigEarthNet(root=data_root,
-                         split='train',
-                         bands='s2',
-                         num_classes=43,
-                         transforms=None,
-                         download=False)
+def load_data(data_root, batch_size=64):
+    train_data = BigEarthNet(root=data_root,
+                             split='train',
+                             bands='s2',
+                             num_classes=43,
+                             transforms=None,
+                             download=False)
 
-dataloader = DataLoader(train_data,
-                        batch_size=batch_size,
-                        shuffle=True)
+    dataloader = DataLoader(train_data,
+                            batch_size=batch_size,
+                            shuffle=True)
+
+    return train_data, dataloader
+
 
 if __name__ == "__main__":
-
+    args = get_args()
+    train_data, dataloader = load_data(data_root=args['data_root'])
     train_dict = next(iter(dataloader))
     print(f"Feature batch shape: {train_dict['image'].size()}")
     print(f"Labels batch shape: {train_dict['label'].size()}")
-    img = train_dict['image'][0].squeeze()[1,:,:]
+    img = train_dict['image'][0].squeeze()[1, :, :]
     label = train_dict['label'][0]
     plt.imshow(img, cmap="gray")
     plt.show()
